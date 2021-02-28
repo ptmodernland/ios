150626bcabfa2d578acf43c1c13690f48abda8b3
Kevin Correzian Arintyo <kevincorrezian@gmail.com>
2021-02-28 20:09:44 +0700

    Initial Commit

---
+++ Modernland Approval/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Modernland Approval
+//
+//  Created by Kevin Correzian on 28/02/21.
+//  Copyright © 2021 Modernland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
