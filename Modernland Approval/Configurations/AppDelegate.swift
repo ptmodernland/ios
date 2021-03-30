c7084fb723e599b8f11fbf2fc15114fc4fcc0390
Kevin Correzian Arintyo <kevincorrezian@gmail.com>
2021-03-30 11:34:08 +0700

    certificate config

--- Modernland Approval/Configurations/AppDelegate.swift
+++ Modernland Approval/Configurations/AppDelegate.swift
@@ -43,7 +43,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         //viewController?.loadRequest(for: deviceTokenString)
         deviceTokenString = deviceToken.map{String(format: "%02,2hhx", $0)}.joined()
         print(deviceTokenString)
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+        } else {
+          let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
     }
+    
 
     internal func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print(error)
